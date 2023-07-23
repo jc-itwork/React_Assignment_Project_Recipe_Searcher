7bfcd3d72340e1b461fd5e92bd39460d32dafdf5
jc-itwork <106012459+jc-itwork@users.noreply.github.com>
2023-07-23 16:20:21 +0200

    Delete src directory

--- src/components/ui/SearchInput.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Input } from "@chakra-ui/react";
-
-export const SearchInput = ({ onChange, ...props }) => {
-  return <Input onChange={onChange} {...props} />;
-};
